15b2186e25847894dc360f3cad908d2c11652e10
Coding Unicorn <co.unicorn.ding@gmail.com>
2021-06-26 17:44:18 +0300

    remove cloudflare worker

--- worker/index.js
+++
@@ -1,8 +0,0 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const response = await fetch(request);
-  return response;
-}
