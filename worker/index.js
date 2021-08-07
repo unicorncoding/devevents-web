5277d0c797d86322befdd2ebdf0c47785fa9bc74
Coding Unicorn <co.unicorn.ding@gmail.com>
2021-08-07 21:11:42 +0300

    redirect Twitter bot to /og/ endpoint

--- worker/index.js
+++ worker/index.js
@@ -3,6 +3,17 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  const response = await fetch(request);
-  return response;
+  const { headers, url } = request
+
+  const userAgent = headers.get('user-agent')
+  if (!userAgent.includes('Twitterbot')) {
+    return await fetch(request)
+  }
+
+  if (!url.includes('dev.events/conferences/')) {
+    return await fetch(request)
+  }
+
+  const redirectUrl = url.replace('/conferences/', '/api/events/og/')
+  return Response.redirect(redirectUrl, 302)
 }
